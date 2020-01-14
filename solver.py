c2a86f983ce64f6e46da2db35936bf6b16f7a7a7
chabotmoe1 <chabotmoe1@gmail.com>
2020-01-13 21:32:47 -0500

    random fixes

--- solver.py
+++ solver.py
@@ -9,6 +9,7 @@ nltk.download('words')
 
 wordlist = words.words()
 
+
 def next_letter(letters1, letters2, len_word, len_word2):
     word1 = ''
     word2 = ''
@@ -23,7 +24,7 @@ def next_letter(letters1, letters2, len_word, len_word2):
     posible_letter = []
     posible_letter2 = []
 
-
+    print(word1,len_word, word2,len_word2)
     for i in wordlist:
         if i.startswith(str(word1)) and len(i) == len_word:
             posible_letter.append(i[len(word1)])
@@ -38,7 +39,7 @@ def next_letter(letters1, letters2, len_word, len_word2):
     return sample
 
 
-def word_map(matrix, direction):
+def word_map_across(matrix):
     matrix = matrix.astype('str')
     down_or_across = []
     for row in matrix:
@@ -57,8 +58,6 @@ def word_map(matrix, direction):
             if count2 == 15:
                 down_or_across.append(count)
 
-    down_or_across = [i for i in down_or_across if i != 0]
-
     down_or_across_2 = []
     for i in down_or_across:
         for j in range(i):
@@ -71,30 +70,66 @@ def word_map(matrix, direction):
     count = 0
 
 
+    for i in range(len(test_1[:, 1])):
+        for j in range(len(test_1[1, :])):
+            if test_1[i, j] != 0:
+                test_1[i, j] = down_or_across_2[count]
+                count += 1
 
-    if direction == 'across':
-        for i in range(len(test_1[:, 1])):
-            for j in range(len(test_1[1, :])):
-                if test_1[i, j] != 0:
-                    test_1[i, j] = down_or_across_2[count]
-                    count += 1
+    return test_1
 
-    else:
-        for i in range(len(test_1[1, :])):
-            for j in range(len(test_1[:, 1])):
-                if test_1[j, i] != 0:
-                    test_1[j, i] = down_or_across_2[count]
-                    count += 1
+
+def word_map_down(matrix):
+    matrix = matrix.astype('str')
+    down_or_across = []
+
+    down = []
+
+    for row in range(15):
+        count = 0
+        count2 = 0
+        for i in matrix[:, row]:
+            count2 += 1
+            if i == '1.0':
+                count += 1
+
+            else:
+                if count > 0:
+                    down.append(count)
+                    count = 0
+
+            if count2 == 15:
+                down.append(count)
+
+    down_or_across_2 = []
+    for i in down:
+        for j in range(i):
+            down_or_across_2.append(i)
+
+    print(down_or_across_2)
+    test_1 = matrix.copy()
+
+    test_1 = test_1.astype('float')
+
+    count = 0
+
+    for i in range(len(matrix[1, :])):
+        for j in range(len(matrix[:, 1])):
+            if test_1[j, i] != 0:
+                test_1[j, i] = down_or_across_2[count]
+                count += 1
 
     return test_1
 
 
 puzzle = get_puzzel()
-puzzle = puzzle.astype('str')[:5]
 
+across_words = word_map_across(puzzle)
+down_words = word_map_down(puzzle)
 
-across_words = word_map(puzzle, 'across')
-down_words = word_map(puzzle, 'down')
+puzzle = puzzle.astype('str')
+puzzle[0,:4] = ['t','i','m','e']
+
 
 
 result = None
@@ -113,6 +148,7 @@ while result is None:
 
                 if puzzle[i, j] == '1.0':
 
+                    # TODO: problem with the second input
                     letter = next_letter(puzzle_copy[i, count0:j], puzzle_copy[count1:i, j],
                                          across_words[i, j], down_words[i, j])[0]
 
@@ -127,7 +163,7 @@ while result is None:
                     count2 += 1
 
         result = puzzle_copy
-        # print(result)
+        print('\n', result)
     except ValueError as e:
         print('\n')
         pass
